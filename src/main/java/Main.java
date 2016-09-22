1457ae9f0b0b3bc35b4a3fb16a9bac10775ca8b0
Alejo <alejojaramillo96@gmail.com>
2016-09-22 13:11:36 +0200

    Hello World

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,9 +18,9 @@ public class Main {
   public static void main(String[] args) {
 
     port(Integer.valueOf(System.getenv("PORT")));
-    staticFileLocation("/public");
+    
 
-    get("/hello", (req, res) -> "Hello World");
+    get("/", (req, res) -> "Hello World");
 
   }
 
