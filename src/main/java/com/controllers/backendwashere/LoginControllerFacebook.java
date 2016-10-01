f64393759a17dfb59c1e106192d4863ef6b25f9d
Irux <alejojaramillo96@gmail.com>
2016-10-01 20:05:26 +0200

    Add new Proof of Concept Login Facebook

---
+++ src/main/java/com/controllers/backendwashere/LoginControllerFacebook.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.controllers.backendwashere;
+
+import spark.Route;
+
+/**
+ *
+ * @author Alejo
+ */
+public class LoginControllerFacebook {
+    
+    
+    public static Route login()
+    {
+        
+        Route loginFacebook = (request,response) -> {
+            
+            
+          return request.body(); 
+          
+          
+        };
+        
+        
+        return loginFacebook;
+    }
+    
+    
+}
