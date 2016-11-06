499fdbbbe9abc881d2c3290590005e48c1e2b953
Alejandro Jaramillo <Alejandro Jaramillo>
2016-11-06 22:33:00 +0100

    Recreating User ORM Mapper

--- src/main/java/com/pojos/backendwashere/User.java
+++ src/main/java/com/pojos/backendwashere/User.java
@@ -11,56 +11,15 @@ package com.pojos.backendwashere;
  */
 public class User {
     
-    private String userFullname;
-    private String user;
-    private String token;
-    private long id;
-    private String profilePicture;
-    private String email;
+    
 
-    public User(String userFullname, String user, String token, long id, String profilePicture) {
-        this.userFullname = userFullname;
-        this.user = user;
-        this.token = token;
-        this.id = id;
-        this.profilePicture = profilePicture;
-        this.email = email;
+    public User() {
     }
 
-    /**
-     * @return the userFullname
-     */
-    public String getUserFullname() {
-        return userFullname;
-    }
+    
 
-    /**
-     * @return the user
-     */
-    public String getUser() {
-        return user;
-    }
-
-    /**
-     * @return the token
-     */
-    public String getToken() {
-        return token;
-    }
-
-    /**
-     * @return the id
-     */
-    public long getId() {
-        return id;
-    }
-
-    /**
-     * @return the profilePicture
-     */
-    public String getProfilePicture() {
-        return profilePicture;
-    }
+    
+    
     
     
     
