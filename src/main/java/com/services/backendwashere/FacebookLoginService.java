aef1aad6c673f3356bbcf21784e11dbb4c1b6e6c
Alejandro Jaramillo <Alejandro Jaramillo>
2016-11-17 16:48:26 +0100

    Implement getServiceToken in FacebookLoginService

--- src/main/java/com/services/backendwashere/FacebookLoginService.java
+++ src/main/java/com/services/backendwashere/FacebookLoginService.java
@@ -51,7 +51,9 @@ public class FacebookLoginService extends LoginServiceToken{
 
     @Override
     public String getServiceToken() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        return super.token;
+
     }
 
    
