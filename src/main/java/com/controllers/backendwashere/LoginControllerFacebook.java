def01fadbb90cfae323fa7a294ec27b249cb0dd5
Alejandro Jaramillo <Alejandro Jaramillo>
2016-11-18 10:06:52 +0100

    Add changes to LoginControllerFB

--- src/main/java/com/controllers/backendwashere/LoginControllerFacebook.java
+++ src/main/java/com/controllers/backendwashere/LoginControllerFacebook.java
@@ -10,92 +10,82 @@ import com.google.gson.JsonParser;
 import com.interfaces.backendwashere.LoginServiceToken;
 import com.pojos.backendwashere.FacebookToken;
 import com.pojos.backendwashere.UserDB;
+import com.pojos.backendwashere.UserTokenAuth;
 import com.restfb.types.User;
 import com.services.backendwashere.FacebookLoginService;
 import java.math.BigInteger;
 import java.util.List;
+import org.eclipse.jetty.security.UserAuthentication;
 import org.javalite.activejdbc.Base;
 import spark.Route;
-
-
 
 /**
  *
  * @author Alejo
  */
-public class LoginControllerFacebook  {
-    
-    
-    public static Route login(LoginServiceToken loginService)
-    {
-        
-        Route loginFacebook = (request,response) -> {
-            
-            
-            String tokenFromCliente = request.body();
-            
-           boolean successful = loginService.login(tokenFromCliente);
-            
-           
-           if(successful)
-           {
-                   Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/washereDB", "root", "TUBerlin2016");
+public class LoginControllerFacebook {
 
-               
-               User userFB = (User)loginService.getLastUserLogin();
-               
-               UserDB userInDB = UserDB.findFirst("id = ?", Long.parseLong(userFB.getId()));
-               
-               System.out.println(userInDB);
-               
-               if(userInDB == null)
-               {
-               try{
-               UserDB user = new UserDB();
-               user.set("tokenFB",loginService.getLastTokenLogin());
-               user.set("idFB",Long.parseLong(userFB.getId()));
-               user.set("first_name",userFB.getFirstName());
-               user.set("last_name",userFB.getLastName());
-               user.set("age_range",userFB.getAgeRange().getMin() + "-" + userFB.getAgeRange().getMax());
-               user.set("gender",userFB.getGender());
-               user.set("locale",userFB.getLocale());
-               user.set("verified",userFB.getVerified().toString());
-               user.set("tokenLogin","test");
-               user.set("picture",userFB.getPicture().getUrl());
-               user.saveIt();
-               
-               
-                   System.err.println(user.errors());
-               
-               
-             
-               
-               }
-               
-               catch(Exception e)
-                       {
-                       System.err.println("Error en la transaccion : " + e.getMessage() + " " + e.getLocalizedMessage());
-                       Base.close();
-                       }
-               
-               }
-               
-               
-               String reponseToken = loginService.getServiceToken();
-               
-               return "{token:WORKS}";
-           }
-           else
-           {
-               return "Error with login Facebook";
-           }
+    public static Route login(LoginServiceToken loginService) {
+
+        Route loginFacebook = (request, response) -> {
+
+            String tokenFromCliente = request.body();
+
+            boolean successful = loginService.login(tokenFromCliente);
+
+            if (successful) {
+                Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/washereDB", "root", "TUBerlin2016");
+
+                User userFB = (User) loginService.getLastUserLogin();
+
+                UserDB userInDB = UserDB.findFirst("id = ?", Long.parseLong(userFB.getId()));
+
+                System.out.println(userInDB);
+
+                if (userInDB == null) {
+                    try {
+                        UserDB user = new UserDB();
+                        user.set("tokenFB", loginService.getLastTokenLogin());
+                        user.set("idFB", Long.parseLong(userFB.getId()));
+                        user.set("first_name", userFB.getFirstName());
+                        user.set("last_name", userFB.getLastName());
+                        user.set("age_range", userFB.getAgeRange().getMin() + "-" + userFB.getAgeRange().getMax());
+                        user.set("gender", userFB.getGender());
+                        user.set("locale", userFB.getLocale());
+                        user.set("verified", userFB.getVerified().toString());
+                        user.set("tokenLogin", "test");
+                        user.set("picture", userFB.getPicture().getUrl());
+                        user.saveIt();
+
+                        System.err.println(user.errors());
+
+                    } catch (Exception e) {
+                        System.err.println("Error en la transaccion : " + e.getMessage() + " " + e.getLocalizedMessage());
+                        Base.close();
+                    }
+
+                    Gson gsonSerialized = new Gson();
+                    UserTokenAuth userAuth = new UserTokenAuth(Long.parseLong(userFB.getId()), (String) loginService.getLastTokenLogin(), 2);
+                    String userAuthString = gsonSerialized.toJson(userAuth);
+                    JsonParser parser = new JsonParser();
+                    return parser.parse("{token:" + userAuthString + "}");
+                } else {
+                    Gson gsonSerialized = new Gson();
+                    UserTokenAuth userAuth = new UserTokenAuth(Long.parseLong(userFB.getId()), (String) loginService.getLastTokenLogin(), 2);
+                    String userAuthString = gsonSerialized.toJson(userAuth);
+                    JsonParser parser = new JsonParser();
+                    return parser.parse("{token:" + userAuthString + "}");
+
+                    //return "Error with login Facebook";
+                }
+
+            }
             
-                      
+            return "Error with login with FB";
+
         };
-        
-        
+
         return loginFacebook;
     }
-    
-    
+
 }
