b1914778a820fa0b0f7ac4d125636ce5f95ab818
Alejandro Jaramillo <Alejandro Jaramillo>
2016-11-24 15:01:24 +0100

    Add ActiveJDBC Model for POST

---
+++ src/main/java/com/pojos/backendwashere/PostDB.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pojos.backendwashere;
+
+import org.javalite.activejdbc.Model;
+
+/**
+ *
+ * @author Alejandro Jaramillo
+ */
+public class PojoDB extends Model {
+    
+    
+    
+    
+    
+}
