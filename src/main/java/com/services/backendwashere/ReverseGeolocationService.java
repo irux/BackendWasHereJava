7e31ec7f46973865f49b80f882eaf26029ab88be
Alejandro Jaramillo <Alejandro Jaramillo>
2017-01-07 16:31:42 +0100

    Adding service from google maps api

--- src/main/java/com/services/backendwashere/ReverseGeolocationService.java
+++ src/main/java/com/services/backendwashere/ReverseGeolocationService.java
@@ -10,11 +10,14 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.maps.GeoApiContext;
+import com.google.maps.GeocodingApi;
+import com.google.maps.model.GeocodingResult;
+import com.google.maps.model.LatLng;
 import com.pojos.backendwashere.PlacePojo;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import jdk.nashorn.internal.parser.JSONParser;
 import okhttp3.*;
 
 /**
@@ -33,62 +36,82 @@ public class ReverseGeolocationService {
     public String getReverseGoogleAPI(float longitude,float latitude)
     {
         
-        String resultAddress = null;
-         Response response = null;
+        GeoApiContext context = new GeoApiContext().setApiKey("AIzaSyAvF8s-D-3K97jsgirefF0bo6YaNB26Fh4");
+        
+        LatLng latitudLongitude = new LatLng(longitude, latitude);
+        
+            GeocodingResult[] result;
         try {
+            result = GeocodingApi.reverseGeocode(context, latitudLongitude).await();
+            System.out.println("Here the formattedAddress : " + result[0].formattedAddress);
+            return result[0].formattedAddress;
+        } catch (Exception ex) {
+            Logger.getLogger(ReverseGeolocationService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return null;
+            
+            
+            /*
+            String resultAddress = null;
+            Response response = null;
+            try {
             OkHttpClient client = new OkHttpClient();
         
             String url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + latitude+"," + longitude + "&key=AIzaSyAvF8s-D-3K97jsgirefF0bo6YaNB26Fh4";
-        Request request = new Request.Builder()
-                
-                .url(url)
-                .build();
-
-        response = client.newCall(request).execute();
-        
-        System.out.println("Response body : " + response.body().string());
-        } catch (Exception e) {
+            Request request = new Request.Builder()
+            
+            .url(url)
+            .build();
+            
+            response = client.newCall(request).execute();
+            
+            System.out.println("Response body : " + response.body().string());
+            } catch (Exception e) {
             System.err.println("Error : " + e.getMessage());
             
-        }
-        
-        System.out.println("Response : " + response);
-        
-        if(response != null)
-        {
-            System.out.println("I am already here");
-        JsonParser parser = new JsonParser();
-            
-        JsonElement objectResult = null;
-            try {
-                System.out.println("I am in try");
-                objectResult = parser.parse(response.body().string());
-                System.out.println(objectResult.toString());
-            } catch (IOException ex) {
-                System.err.println("Error : " + ex.getMessage());
             }
-       
+            
+            System.out.println("Response : " + response);
+            
+            if(response != null)
+            {
+            System.out.println("I am already here");
+            JsonParser parser = new JsonParser();
+            
+            JsonElement objectResult = null;
+            try {
+            System.out.println("I am in try");
+            objectResult = parser.parse(response.body().string());
+            System.out.println(objectResult.toString());
+            } catch (IOException ex) {
+            System.err.println("Error : " + ex.getMessage());
+            }
+            
             
             /*
             System.out.println("Object result : " + objectResult);
             
-        JsonArray resultArray = objectResult.getAsJsonArray("results");
-        
+            JsonArray resultArray = objectResult.getAsJsonArray("results");
+            
             System.out.println("Result Arrays as string : " + resultArray.toString());
-        
-        JsonObject resultFirst = resultArray.get(0).getAsJsonObject();
-        
+            
+            JsonObject resultFirst = resultArray.get(0).getAsJsonObject();
+            
             System.out.println("resultfirst as array : " + resultFirst);
-        
-        resultAddress = resultFirst.get("formatted_address").getAsString();
-        
-        
+            
+            resultAddress = resultFirst.get("formatted_address").getAsString();
+            
+            
             System.out.println("result Address : " + resultAddress);
-
-                */
+            
+            */
+            
         
-        }
-        return resultAddress;
+        
+        
+        
+        
     }
     
     public PlacePojo getReverseOpenStreetmap(float longitude,float latitude)
